ba7a8f9371b26c01f64bcead511ef5b1ffa64de9
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-17 15:42:54 +0700

    sefl host upload file

--- dist/create_option_advanced_installer_build.ps1
+++ dist/create_option_advanced_installer_build.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = 'Stop'
 $PathConfig = $PSCommandPath + '.action.config.json'
 $Config = Get-Content -Path $PathConfig | ConvertFrom-Json
 $Config.PathExecuteVersion = (Join-Path -Path $PSScriptRoot -ChildPath $Config.PathExecuteVersion -Resolve)
+$Config.PathRcloneZip = (Join-Path -Path $PSScriptRoot -ChildPath $Config.PathRcloneZip -Resolve)
 #Resolve Path file in Config
 try {
     $Config.PathAdvancedInstallerCommandFile = (Join-Path -Path $PSScriptRoot -ChildPath $Config.PathAdvancedInstallerCommandFile -Resolve)
@@ -34,13 +35,16 @@ $Config | Add-Member -NotePropertyName Version -NotePropertyValue $advVersion
 $Config | Add-Member -NotePropertyName PathAdvancedInstallerOutputFile -NotePropertyValue (join-Path -Path $Config.PathAdvancedInstallerOutputFolder -ChildPath $Config.OutputPackageName)
 $Config | Add-Member -NotePropertyName PathAdvancedInstallerOutputFileZip -NotePropertyValue ((join-Path -Path $Config.PathAdvancedInstallerOutputFolder -ChildPath $Config.OutputPackageName.Replace('.exe', '')) + '.v{0}.zip' -f $fileVersion.FileVersion)
 $Config | Add-Member -NotePropertyName GITHUBREPOSITORYSECRETSDEFAULTRTDB -NotePropertyValue ($env:GITHUBREPOSITORYSECRETSDEFAULTRTDB)
-$Config | Add-Member -NotePropertyName PathRcloneConfig -NotePropertyValue ($PSScriptRoot + '\' + 'rclone.conf')
+$Config | Add-Member -NotePropertyName PathRcloneConfig -NotePropertyValue ($PSScriptRoot + '\rclone.conf')
+$Config | Add-Member -NotePropertyName PathRclone -NotePropertyValue ($PSScriptRoot + '\rclone.exe')
+$Config | Add-Member -NotePropertyName PathRcloneFolder -NotePropertyValue ($PSScriptRoot + '\')
 
 if ([string]::IsNullOrEmpty($Config.GITHUBREPOSITORYSECRETSDEFAULTRTDB)) {
     $Config.GITHUBREPOSITORYSECRETSDEFAULTRTDB = (Get-Content -Path ($PSScriptRoot + '\' + 'GITHUBREPOSITORYSECRETSDEFAULTRTDB.githubignore'))
 }
 $Config | Add-Member -NotePropertyName GITHUBREPOSITORYSECRETSDEFAULTRTDB_DECODE -NotePropertyValue ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Config.GITHUBREPOSITORYSECRETSDEFAULTRTDB)))
-$Config.GITHUBREPOSITORYSECRETSDEFAULTRTDB_DECODE | Out-File -FilePath $Config.PathRcloneConfig
+#$Config.GITHUBREPOSITORYSECRETSDEFAULTRTDB_DECODE | Out-File -FilePath $Config.PathRcloneConfig
+[System.IO.File]::WriteAllLines($Config.PathRcloneConfig, $Config.GITHUBREPOSITORYSECRETSDEFAULTRTDB_DECODE, (New-Object System.Text.UTF8Encoding $False))
 #Write to AdvancedInstaller commandFile
 $saveFile = @(';aic', `
     ('SetVersion ' + $Config.Version), `
@@ -50,7 +54,26 @@ $saveFile = @(';aic', `
         'Save', `
         'Rebuild')
 $saveFile | Out-File -FilePath $Config.PathAdvancedInstallerCommandFile
-Write-Host $Config
-return
 &AdvancedInstaller.com /execute $Config.PathAdvancedInstallerProjectFile $Config.PathAdvancedInstallerCommandFile 
 compress-archive -path $Config.PathAdvancedInstallerOutputFile -destinationpath ($Config.PathAdvancedInstallerOutputFileZip) -Force
+if ([System.IO.File]::Exists($Config.PathRclone)) { [System.IO.File]::Delete($Config.PathRclone) }
+expand-archive -path $Config.PathRcloneZip -destinationpath $Config.PathRcloneFolder
+if ([System.IO.File]::Exists($Config.PathRclone) -and 
+    [System.IO.File]::Exists($Config.PathRcloneConfig)) {
+    $Config | Add-Member -NotePropertyName RcloneArgumentList -NotePropertyValue ('')
+             
+    while ($Config.RcloneCloudPath.StartsWith('\')) { $Config.RcloneCloudPath = $Config.RcloneCloudPath.TrimStart('\') }      
+    while ($Config.RcloneCloudPath.EndsWith('\')) { $Config.RcloneCloudPath = $Config.RcloneCloudPath.TrimEnd('\') }    
+    Get-Content $Config.PathRcloneConfig | ForEach-Object {
+        if ($_.StartsWith('[') -and $_.EndsWith(']')) {
+            $uploadName = $_.Replace('[', '').Replace(']', '')
+            $Config.RcloneArgumentList = '' 
+            $Config.RcloneArgumentList += 'copy "' + $Config.PathAdvancedInstallerOutputFileZip + '" ' + $uploadName + ':"' + $Config.RcloneCloudPath + '"'
+            $Config.RcloneArgumentList += ' --config "' + $Config.PathRcloneConfig + '"'
+            $Config.RcloneArgumentList += ' --auto-confirm'
+            Write-Host ('Rclone upload: {0}=>{1}' -f $Config.PathAdvancedInstallerOutputFileZip, $uploadName)
+            Start-Process -WindowStyle Hidden -Wait -FilePath $Config.PathRclone -ArgumentList $Config.RcloneArgumentList
+        }
+    }  
+}
+Write-Host $Config
